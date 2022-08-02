db9f49aafc72a0f8ab60e7e8c4b567574fd0815d
David Torralba Goitia <david.torralba.goitia@gmail.com>
2022-08-02 18:26:51 +0100

    It works!! read commit description
    Steps to make it work:
    
      1. Remove any old files:
    
        ```shell
        rm -rf PKGBUILD_files/TO_DELETE/
        ```
    
      2. Spin up a container by running:
    
        ```shell
        docker-compose down
        make rebuild_container_image shell_in_container
        ```
    
      3. Inside the container, build: `bash build.sh`
    
      4. Inside the container, install: `bash install.sh`
    
    `direnvbackup` command should be now available to you globally!

--- PKGBUILD_files/build.sh
+++ PKGBUILD_files/build.sh
@@ -5,9 +5,14 @@ template=PKGBUILD.template
 
 cp $template $pkgbuild
 
+work_dir="TO_DELETE"
+mkdir $work_dir
+cp $pkgbuild "$work_dir/$pkgbuild"
+cd $work_dir
+
 # Update checksum
 makepkg -g >> $pkgbuild
 
 # Build package
-makepkg --syncdeps --force --rmdeps
+makepkg --syncdeps --force
 # TODO: use --rmdeps to clean up makedeps once its done
---
+++ PKGBUILD_files/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo pacman -U TO_DELETE/direnv-backup-git-0.0.1-1-any.pkg.tar.zst
